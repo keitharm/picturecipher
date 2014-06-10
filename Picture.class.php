326633fa7c5105222c46651f87e15ce18ab9f987
Keith Armstrong <keith@randomapi.com>
2014-06-10 08:36:40 -0500

    Converted indentations to spaces

--- Picture.class.php
+++ Picture.class.php
@@ -3,222 +3,222 @@ require_once("Text.class.php");
 
 class Picture
 {
-	private $input;
-	private $password;
-	private $text;
-	private $binary;
-	private $output;
+    private $input;
+    private $password;
+    private $text;
+    private $binary;
+    private $output;
 
-	public static function encrypt($input, $password = null) {
-		$instance = new self();
+    public static function encrypt($input, $password = null) {
+        $instance = new self();
 
-		// Set input and password
-		$instance->input     = $input;
-		$instance->password  = $password;
+        // Set input and password
+        $instance->input     = $input;
+        $instance->password  = $password;
 
-		// Create Text object using input and password
-		$instance->text      = Text::encrypt($instance->getInput(), $instance->getPassword());
+        // Create Text object using input and password
+        $instance->text      = Text::encrypt($instance->getInput(), $instance->getPassword());
 
-		// Convert Text object output into binary
-		$instance->binary    = $instance->toBin($instance->getText()->getOutput());
+        // Convert Text object output into binary
+        $instance->binary    = $instance->toBin($instance->getText()->getOutput());
 
-		return $instance;
-	}
+        return $instance;
+    }
 
-	public static function decrypt($input, $password = null) {
-		$instance = new self();
+    public static function decrypt($input, $password = null) {
+        $instance = new self();
 
-		// Set input and password
-		$instance->input    = $input;
-		$instance->password = $password;
+        // Set input and password
+        $instance->input    = $input;
+        $instance->password = $password;
 
-		// Convert image into binary
-		$instance->binary   = $instance->decodeImage();
+        // Convert image into binary
+        $instance->binary   = $instance->decodeImage();
 
-		// Convert binary to Text object
-		$instance->text     = Text::decrypt($instance->fromBin($instance->getBinary()), $instance->getPassword());
+        // Convert binary to Text object
+        $instance->text     = Text::decrypt($instance->fromBin($instance->getBinary()), $instance->getPassword());
 
-		// Decrypt text object to original input
-		$instance->output   = $instance->getText()->getOutput();
+        // Decrypt text object to original input
+        $instance->output   = $instance->getText()->getOutput();
 
-		return $instance;
-	}
+        return $instance;
+    }
 
-	public function outputImage() {
-		$chunks = $this->getChunks();
+    public function outputImage() {
+        $chunks = $this->getChunks();
 
-		// Add extra chunks to make total chuncks divisible by 3 for complete RGB pixels
-		while (count($chunks) % 3 != 0) {
-			$chunks[] = "00000000";
-		}
+        // Add extra chunks to make total chuncks divisible by 3 for complete RGB pixels
+        while (count($chunks) % 3 != 0) {
+            $chunks[] = "00000000";
+        }
 
-		// Calculate dimensions of image
-		$dim = ceil(sqrt(count($chunks) / 3));
-		$img = imagecreatetruecolor($dim, ceil(count($chunks) / $dim / 3));
-		$colors = array();
+        // Calculate dimensions of image
+        $dim = ceil(sqrt(count($chunks) / 3));
+        $img = imagecreatetruecolor($dim, ceil(count($chunks) / $dim / 3));
+        $colors = array();
 
-		// Convert groups of 3 chunks into pixels
-		for ($a = 0; $a < count($chunks); $a += 3) {
-		    array_push($colors, imagecolorallocate($img, bindec($chunks[$a]), bindec($chunks[$a+1]), bindec($chunks[$a+2])));
-		}
+        // Convert groups of 3 chunks into pixels
+        for ($a = 0; $a < count($chunks); $a += 3) {
+            array_push($colors, imagecolorallocate($img, bindec($chunks[$a]), bindec($chunks[$a+1]), bindec($chunks[$a+2])));
+        }
 
-		// Set color for each pixel
-		for ($j = 0; $j < $dim; $j++) {
-	        for ($i = 0; $i < $dim; $i++) {
-	            imagesetpixel($img, $i, $j, $colors[($j*$dim)+$i]);
-	        }
-	    }
+        // Set color for each pixel
+        for ($j = 0; $j < $dim; $j++) {
+            for ($i = 0; $i < $dim; $i++) {
+                imagesetpixel($img, $i, $j, $colors[($j*$dim)+$i]);
+            }
+        }
 
-	    // Output image
-		imagepng($img);
-		imagedestroy($img);
-	}
+        // Output image
+        imagepng($img);
+        imagedestroy($img);
+    }
 
-	public function decodeImage() {
-	    // Array filled with information regarding the image
-	    $imageinfo = getimagesize($this->getInput());
+    public function decodeImage() {
+        // Array filled with information regarding the image
+        $imageinfo = getimagesize($this->getInput());
 
-	    // Height in pixels
-	    $height = $imageinfo[1];
+        // Height in pixels
+        $height = $imageinfo[1];
 
-	    // Width in pixels
-	    $width = $imageinfo[0];
+        // Width in pixels
+        $width = $imageinfo[0];
 
-	    // Create image
-	    $im = @imagecreatefrompng($this->getInput());
+        // Create image
+        $im = @imagecreatefrompng($this->getInput());
 
-	    // Finds the index of the color of each pixel in the image
-		for ($j = 0; $j < $height; $j++) {
-			for ($i = 0; $i < $width; $i++) {
-				// Adds the index of the colors into the $chars array
-				$chars[] = imagecolorsforindex($im, imagecolorat($im, $i, $j));
-	        }
-	    }
+        // Finds the index of the color of each pixel in the image
+        for ($j = 0; $j < $height; $j++) {
+            for ($i = 0; $i < $width; $i++) {
+                // Adds the index of the colors into the $chars array
+                $chars[] = imagecolorsforindex($im, imagecolorat($im, $i, $j));
+            }
+        }
 
-	    // Push RGB pixel colors into values array
-	    for ($a = 0; $a < count($chars); $a++) {
+        // Push RGB pixel colors into values array
+        for ($a = 0; $a < count($chars); $a++) {
             if ($chars[$a]["red"] == 0 && $chars[$a]["green"] == 0 && $chars[$a]["blue"] == 0) {
                 continue;
             } else {
-		        $values[] = $chars[$a]["red"];
-		        $values[] = $chars[$a]["green"];
-		        $values[] = $chars[$a]["blue"];
-		    }
-	    }
+                $values[] = $chars[$a]["red"];
+                $values[] = $chars[$a]["green"];
+                $values[] = $chars[$a]["blue"];
+            }
+        }
 
-	    // Convert each pixel color from decimal to it's binary value
-	    for ($a = 0; $a < count($values); $a++) {
-	        $result .= sprintf("%08d", decbin($values[$a]));
-	    }
-	    return $result;
-	}
+        // Convert each pixel color from decimal to it's binary value
+        for ($a = 0; $a < count($values); $a++) {
+            $result .= sprintf("%08d", decbin($values[$a]));
+        }
+        return $result;
+    }
 
-	public function charToInt($char) {
-		return array_search($char, $this->chars());
-	}
+    public function charToInt($char) {
+        return array_search($char, $this->chars());
+    }
 
-	public function intToChar($int) {
-		return $this->chars()[$int];
-	}
+    public function intToChar($int) {
+        return $this->chars()[$int];
+    }
 
-	private function toBin($content) {
-		// Split encrypted text into array;
-		$split = str_split($content);
+    private function toBin($content) {
+        // Split encrypted text into array;
+        $split = str_split($content);
 
-		// First 3 characters of string are offset.
-		// 000 in beginning until we calculate the
-		// offset. Then it is updated.
-		$str = "000";
+        // First 3 characters of string are offset.
+        // 000 in beginning until we calculate the
+        // offset. Then it is updated.
+        $str = "000";
 
-		// Convert ascii characters into binary using charToInt mapping
-		foreach ($split as $val) {
-			$str .= sprintf("%06d", decbin($this->charToInt($val)));
-		}
+        // Convert ascii characters into binary using charToInt mapping
+        foreach ($split as $val) {
+            $str .= sprintf("%06d", decbin($this->charToInt($val)));
+        }
 
-		// Calculate offset for padding in order to make last byte an octet
-		$offset = sprintf("%03d", decbin(8 - (strlen($str) % 8)));
+        // Calculate offset for padding in order to make last byte an octet
+        $offset = sprintf("%03d", decbin(8 - (strlen($str) % 8)));
 
-		// Replace 1st 3 characters with offset
-		$str = substr_replace($str, $offset, 0, 3);
+        // Replace 1st 3 characters with offset
+        $str = substr_replace($str, $offset, 0, 3);
 
-		// Fill rest of last chunk with zeros as padding
-		$str .= str_repeat("0", 8 - (strlen($str) % 8));
+        // Fill rest of last chunk with zeros as padding
+        $str .= str_repeat("0", 8 - (strlen($str) % 8));
 
-		// Return binary string
-		return $str;
-	}
+        // Return binary string
+        return $str;
+    }
 
-	private function fromBin($content) {
-		$offset = substr($content, 0, 3);
-		$remove = bindec($offset);
-		// Remove the zero padding (if it exists)
-		#echo $content . "\n";
-		if ($remove == 0) {
-			$new = substr($content, 3);
-		} else {
-			$new = substr(substr($content, 0, -$remove), 3);
-		}
+    private function fromBin($content) {
+        $offset = substr($content, 0, 3);
+        $remove = bindec($offset);
+        // Remove the zero padding (if it exists)
+        #echo $content . "\n";
+        if ($remove == 0) {
+            $new = substr($content, 3);
+        } else {
+            $new = substr(substr($content, 0, -$remove), 3);
+        }
 
-		// Remove 1st 3 characters for padding calculation
-		$chars = explode(" ", chunk_split($new, 6, " "));
+        // Remove 1st 3 characters for padding calculation
+        $chars = explode(" ", chunk_split($new, 6, " "));
 
-		// Unset last empty char chunk
-		unset($chars[count($chars)-1]);
+        // Unset last empty char chunk
+        unset($chars[count($chars)-1]);
 
-		foreach ($chars as $char) {
-			$str .= $this->intToChar(bindec($char));
-		}
+        foreach ($chars as $char) {
+            $str .= $this->intToChar(bindec($char));
+        }
 
-		// Return string
-		return $str;
-	}
+        // Return string
+        return $str;
+    }
 
-	private function chars() {
-		$chars = array(
-			'A', 'B', 'C', 'D', 'E',
-			'F', 'G', 'H', 'I', 'J',
-			'K', 'L', 'M', 'N', 'O',
-			'P', 'Q', 'R', 'S', 'T',
-			'U', 'V', 'W', 'X', 'Y',
-			'Z', 'a', 'b', 'c', 'd',
-			'e', 'f', 'g', 'h', 'i',
-			'j', 'k', 'l', 'm', 'n',
-			'o', 'p', 'q', 'r', 's',
-			't', 'u', 'v', 'w', 'x',
-			'y', 'z', '0', '1', '2',
-			'3', '4', '5', '6', '7',
-			'8', '9', '+', '/'
-		);
-		return $chars;
-	}
+    private function chars() {
+        $chars = array(
+            'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y',
+            'Z', 'a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x',
+            'y', 'z', '0', '1', '2',
+            '3', '4', '5', '6', '7',
+            '8', '9', '+', '/'
+        );
+        return $chars;
+    }
 
-	public function getChunks() {
-		// Split binary string into byte chunks
-		$chunks = explode(" ", chunk_split($this->getBinary(), 8, " "));
+    public function getChunks() {
+        // Split binary string into byte chunks
+        $chunks = explode(" ", chunk_split($this->getBinary(), 8, " "));
 
-		// Unset null byte
-		unset($chunks[count($chunks)-1]);
+        // Unset null byte
+        unset($chunks[count($chunks)-1]);
 
-		// Reindex array
-		$chunks = array_values($chunks);
+        // Reindex array
+        $chunks = array_values($chunks);
 
-		return $chunks;
-	}
+        return $chunks;
+    }
 
-	// Getters
-	public function getInput() { return $this->input; }
-	public function getPassword() { return $this->password; }
-	public function getText() { return $this->text; }
-	public function getBinary() { return $this->binary; }
-	public function getMedium() { return $this->medium; }
-	public function getOutput() { return $this->output; }
+    // Getters
+    public function getInput() { return $this->input; }
+    public function getPassword() { return $this->password; }
+    public function getText() { return $this->text; }
+    public function getBinary() { return $this->binary; }
+    public function getMedium() { return $this->medium; }
+    public function getOutput() { return $this->output; }
 
-	// Setters
-	private function setInput($val) { $this->input = $val; }
-	private function setPassword($val) { $this->password = $val; }
-	private function setText($val) { $this->text = $val; }
-	private function setBinary($val) { $this->binary = $val; }
-	private function setMedium($val) { $this->medium = $val; }
-	private function setOutput($val) { $this->output = $val; }
+    // Setters
+    private function setInput($val) { $this->input = $val; }
+    private function setPassword($val) { $this->password = $val; }
+    private function setText($val) { $this->text = $val; }
+    private function setBinary($val) { $this->binary = $val; }
+    private function setMedium($val) { $this->medium = $val; }
+    private function setOutput($val) { $this->output = $val; }
 }
 ?>
--- Text.class.php
+++ Text.class.php
@@ -1,85 +1,85 @@
 <?php
 class Text
 {
-	// Characters used in Base 64
-	const ORIGINAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    // Characters used in Base 64
+    const ORIGINAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-	private $input;
-	private $password;
-	private $cipher;
-	private $base64;
-	private $output;
+    private $input;
+    private $password;
+    private $cipher;
+    private $base64;
+    private $output;
 
-	public static function encrypt($input, $password = null) {
-		$instance = new self();
+    public static function encrypt($input, $password = null) {
+        $instance = new self();
 
-		$instance->input    = $input;
-		$instance->password = $password;
-		$instance->scramble();
-		$instance->base64   = $instance->encode($instance->getInput());
-		$instance->output   = $instance->swap($instance->getBase64(), $instance->getOriginal(), $instance->getCipher());
+        $instance->input    = $input;
+        $instance->password = $password;
+        $instance->scramble();
+        $instance->base64   = $instance->encode($instance->getInput());
+        $instance->output   = $instance->swap($instance->getBase64(), $instance->getOriginal(), $instance->getCipher());
 
-		return $instance;
-	}
+        return $instance;
+    }
 
-	public static function decrypt($input, $password = null) {
-		$instance = new self();
+    public static function decrypt($input, $password = null) {
+        $instance = new self();
 
-		$instance->input    = $input;
-		$instance->password = $password;
-		$instance->scramble();
-		$instance->base64   = $instance->swap($instance->getInput(), $instance->getCipher(), $instance->getOriginal());
-		$instance->output   = $instance->decode($instance->getBase64());
+        $instance->input    = $input;
+        $instance->password = $password;
+        $instance->scramble();
+        $instance->base64   = $instance->swap($instance->getInput(), $instance->getCipher(), $instance->getOriginal());
+        $instance->output   = $instance->decode($instance->getBase64());
 
-		return $instance;
-	}
+        return $instance;
+    }
 
-	private function scramble() {
-		// Seed random number generator
-		srand(hexdec(substr(md5($this->password), 0, 10)));
+    private function scramble() {
+        // Seed random number generator
+        srand(hexdec(substr(md5($this->password), 0, 10)));
 
-		// Shuffle base 64 character string
-    	$this->setCipher(str_shuffle($this->getOriginal()));
-	}
+        // Shuffle base 64 character string
+        $this->setCipher(str_shuffle($this->getOriginal()));
+    }
 
-	private function encode($content) {
-		return base64_encode($content);
-	}
+    private function encode($content) {
+        return base64_encode($content);
+    }
 
-	private function decode($content) {
-		return base64_decode($content);
-	}
+    private function decode($content) {
+        return base64_decode($content);
+    }
 
-	private function swap($text, $original, $new) {
-		// Replace base64 string with chars from scrambled original
-		$len = strlen($text);
+    private function swap($text, $original, $new) {
+        // Replace base64 string with chars from scrambled original
+        $len = strlen($text);
 
-	    for ($a = 0; $a < $len; $a++) {
-	        for ($b = 0; $b < 65; $b++) {
-	            if ($original[$b] == $text[$a]) {
-	                $result .= $new[$b];
-	            }
-	        }
-	    }
+        for ($a = 0; $a < $len; $a++) {
+            for ($b = 0; $b < 65; $b++) {
+                if ($original[$b] == $text[$a]) {
+                    $result .= $new[$b];
+                }
+            }
+        }
 
-	    // Return result
-	    return $result;
-	}
+        // Return result
+        return $result;
+    }
 
-	public function getOriginal() { return Text::ORIGINAL; }
+    public function getOriginal() { return Text::ORIGINAL; }
 
-	// Getters
-	public function getInput() { return $this->input; }
-	public function getPassword() { return $this->password; }
-	public function getCipher() { return $this->cipher; }
-	public function getBase64() { return $this->base64; }
-	public function getOutput() { return $this->output; }
+    // Getters
+    public function getInput() { return $this->input; }
+    public function getPassword() { return $this->password; }
+    public function getCipher() { return $this->cipher; }
+    public function getBase64() { return $this->base64; }
+    public function getOutput() { return $this->output; }
 
-	// Setters
-	private function setInput($val) { $this->input = $val; }
-	private function setPassword($val) { $this->password = $val; }
-	private function setCipher($val) { $this->cipher = $val; }
-	private function setBase64($val) { $this->base64 = $val; }
-	private function setOutput($val) { $this->output = $val; }
+    // Setters
+    private function setInput($val) { $this->input = $val; }
+    private function setPassword($val) { $this->password = $val; }
+    private function setCipher($val) { $this->cipher = $val; }
+    private function setBase64($val) { $this->base64 = $val; }
+    private function setOutput($val) { $this->output = $val; }
 }
 ?>
